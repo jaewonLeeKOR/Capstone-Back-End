4a5bcea328bb576b587fe3507b1921afaa91e191
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 02:10:58 +0900

    feat(FileController.java) #12 : 파일 저장 API 구현

--- src/main/java/com/inha/capstone/controller/FileController.java
+++ src/main/java/com/inha/capstone/controller/FileController.java
@@ -11,6 +11,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
 
 @Slf4j
 @RequestMapping("/file")
@@ -20,6 +23,19 @@ public class FileController {
   private final FileService fileService;
   private final UserService userService;
   private final ApplicationService applicationService;
+
+  @PostMapping("/upload")
+  public ResponseEntity<BaseResponse<String>> uploadFile (
+      Principal principal,
+      @RequestPart(value = "file") MultipartFile multipartFile,
+      @RequestPart(value = "info") PostFileRequest request
+  ) {
+    User user = userService.findByUserId(principal.getName());
+    String s3FilePath = fileService.saveFile(multipartFile, request.getFileCategory(), request.getApplicationId(), request.getUserId(), request.getComponentId(), user);
+    log.info("uploadFile REQUESTED - applicationId : " + request.getApplicationId()+ ",userId : " + request.getUserId() + ",componentId : " + request.getComponentId() + ",fileCategory : " + request.getFileCategory());
+    return ResponseEntity.ok().body(new BaseResponse(s3FilePath));
+  }
+
   @DeleteMapping("/delete")
   public ResponseEntity<BaseResponse<Boolean>> deleteFile(@RequestBody DeleteFileRequest request) {
     User user = userService.findOne(request.getUserId());
