fea44983b860ebd6b55e6c585c8a6ff9cce36b40
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 01:47:41 +0900

    feat #12 : 예외 Status 추가

--- src/main/java/com/inha/capstone/config/BaseResponseStatus.java
+++ src/main/java/com/inha/capstone/config/BaseResponseStatus.java
@@ -14,7 +14,10 @@ public enum BaseResponseStatus {
     INVALID_PARAMETER(false, HttpStatus.BAD_REQUEST, "필수파라미터가 누락됐습니다."),
     DUPLICATED_USER(false, HttpStatus.CONFLICT, "중복된 회원입니다."),
     NOT_EXIST_USER(false, HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
-    NOT_EXIST_APPLICATION(false,HttpStatus.NOT_FOUND, "존재하지 않는 애플리케이션입니다.")
+    NOT_EXIST_APPLICATION(false,HttpStatus.NOT_FOUND, "존재하지 않는 애플리케이션입니다."),
+    CONVERT_MULTIPART_FILE_FAILED(false, HttpStatus.SERVICE_UNAVAILABLE, "MultiPartFile 변환 실패"),
+    INCORRECT_APPLICATIONID(false, HttpStatus.NO_CONTENT,"ApplicationId 는 0이 될 수 없습니다."),
+    CANNOT_CREATE_FILE(false, HttpStatus.CONFLICT,"서버상에 임시파일을 생성할 수 없음");
     ;
     private final boolean isSuccess;
     private final HttpStatus httpStatus;
