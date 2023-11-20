ba26796d91e5f7799e5457013177484526d7913c
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 01:53:26 +0900

    feat(FileDto.java) #12 : 파일 업로드 관련 dto 추가

--- src/main/java/com/inha/capstone/Dto/FIleDto.java
+++ src/main/java/com/inha/capstone/Dto/FIleDto.java
@@ -1,4 +1,35 @@
 package com.inha.capstone.Dto;
 
+import com.inha.capstone.domain.FileCategory;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
 public class FIleDto {
+  /**
+   * 파일 메타데이터 저장을 위한 request DTO
+   * @FileCategory fileCategory
+   * @Long applicationId
+   * @Long userId
+   * @Long componentId
+   */
+  @Data
+  public static class PostFileRequest {
+    @NotBlank
+    FileCategory fileCategory;
+    @NotBlank
+    Long applicationId;
+    @NotBlank
+    Long userId;
+    @NotBlank
+    Long componentId;
+  }
+
+  /**
+   * 파일 저장 후 url 반환을 위한 response DTO
+   * @String filePath
+   */
+  @Data
+  public static class PostFileResponse {
+    String filePath;
+  }
 }
