8e5b02d1402bdc137fb4956263aa5ac3b6c5ce57
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 02:14:23 +0900

    feat(FileRepository.java) #12 : 애플리케이션 내 개인 파일 조회 API 구현

--- src/main/java/com/inha/capstone/repository/FileRepository.java
+++ src/main/java/com/inha/capstone/repository/FileRepository.java
@@ -4,9 +4,33 @@ import com.inha.capstone.domain.Application;
 import com.inha.capstone.domain.FileObject;
 import com.inha.capstone.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FileRepository extends JpaRepository<FileObject, Long> {
+  /**
+   * application의 사용자별 이미지 데이터 조회
+   * application 내 공용 이미지, 개인 이미지 통합
+   * @param application
+   * @param user
+   * @return
+   */
+  @Query("SELECT " +
+            "f " +
+          "FROM " +
+            "FileObject f " +
+          "WHERE " +
+            "f.application = :application " +
+              "AND " +
+            "( " +
+              "f.user = :user " +
+                "OR " +
+              "f.user IS NULL " +
+      ")"
+  )
+  List<FileObject> findFileObjectsByApplicationAndUser(Application application, User user);
+
   Optional<FileObject> findFileObjectByApplicationAndUserAndComponentId(Application application, User user, Long componentId);
 }
