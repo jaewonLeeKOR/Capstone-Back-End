a2c179f786d5af37071f81914d640e38abcc83c2
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 02:07:21 +0900

    feat(FileController.java) #12 : 파일 제거 API 구현

--- src/main/java/com/inha/capstone/controller/FileController.java
+++ src/main/java/com/inha/capstone/controller/FileController.java
@@ -1,8 +1,15 @@
 package com.inha.capstone.controller;
 
+import com.inha.capstone.Dto.FIleDto.*;
+import com.inha.capstone.config.BaseResponse;
+import com.inha.capstone.domain.Application;
+import com.inha.capstone.domain.User;
+import com.inha.capstone.service.ApplicationService;
 import com.inha.capstone.service.FileService;
+import com.inha.capstone.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -11,5 +18,15 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class FileController {
   private final FileService fileService;
+  private final UserService userService;
+  private final ApplicationService applicationService;
+  @DeleteMapping("/delete")
+  public ResponseEntity<BaseResponse<Boolean>> deleteFile(@RequestBody DeleteFileRequest request) {
+    User user = userService.findOne(request.getUserId());
+    Application application = applicationService.findById(request.getApplicationId());
+    fileService.deleteFile(application, user, request.getComponentId());
+    log.info("deleteFile REQUESTED - applicationId : " + application.getApplicationId() + ", userId : " + user.getId() + ",componentId : " + request.getComponentId());
+    return ResponseEntity.ok().body(new BaseResponse<>(true));
+  }
 }
 
