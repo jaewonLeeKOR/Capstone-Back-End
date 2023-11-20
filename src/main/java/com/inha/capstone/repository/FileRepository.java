1d3405c84aed4a1c4a1205c1f024115956976420
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2023-11-21 02:00:52 +0900

    feat(FileRepository.java) #12 : FileObject 조회 메서드 추가

--- src/main/java/com/inha/capstone/repository/FileRepository.java
+++ src/main/java/com/inha/capstone/repository/FileRepository.java
@@ -1,7 +1,12 @@
 package com.inha.capstone.repository;
 
+import com.inha.capstone.domain.Application;
 import com.inha.capstone.domain.FileObject;
+import com.inha.capstone.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileRepository extends JpaRepository<FileObject, Long> {
+  Optional<FileObject> findFileObjectByApplicationAndUserAndComponentId(Application application, User user, Long componentId);
 }
